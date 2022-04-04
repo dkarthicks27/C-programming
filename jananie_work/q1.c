606f3845a90f46b6f3296be03f95f65476adf36e
dkarthicks27 <dkarthicks27@gmail.com>
2022-04-04 18:15:30 +0530

    changes done to jananie's first commit

--- jananie_work/q1.c
+++ jananie_work/q1.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-void main()
+int main()
 {
    int n,i;
-   printf("Enter the number");
+   printf("Enter the number: ");
    scanf("%d",&n);
-   for(i=0;i<100;i++)
-   printf("%d * %d=%d\n",n,i,n*i);
-   
+   for(i=0;i<=10;i++)
+   printf("%d * %d = %d\n",n,i,n*i);
+   return 0;
 }
